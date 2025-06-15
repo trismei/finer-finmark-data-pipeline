97482700a01863ec00c8712c218f963703cdf748
zephyrstorm123 <107976319+zephyrstorm123@users.noreply.github.com>
2025-06-15 11:06:45 +0800

    Added Conversion Functions of datetime for All datasets

--- clean_data_main.py
+++ clean_data_main.py
@@ -2,46 +2,43 @@ import pandas as pd
 import numpy as np
 import sys
 import os
-import clean_event_logs
+import functions.clean_event_logs as clean_event_logs
+import functions.clean_marketing_summary as clean_marketing_summary
+import functions.clean_trend_report as clean_trend_report
 import datetime
 
 def import_data(file_path):
 
-    # Read the dataset
-    df = pd.read_csv(file_path, header=None)
+    dataframe = sys.argv[1]
 
     #Check file to categorize it
     filename = os.path.basename(file_path)
 
     if 'event_log' in filename:
-        slice_column = 5
-        df = df.iloc[:, :slice_column]
-
-        clean_data = clean_event_logs.clean_event_logs(df)
+        clean_data = clean_event_logs.clean_event_logs(dataframe)[1:]
         # Ensure the output directory exists
-        output_dir = os.path.join("datasets", "cleaned")
-        os.makedirs(output_dir, exist_ok=True)
-        output_path = os.path.join(output_dir, f"{os.path.basename(file_path)}_cleaned_{datetime.date.today()}.csv")
-        clean_data.to_csv(output_path, index=False, header=False)
+        output_path = standardize_output_filename(file_path)
+        clean_data.to_csv(output_path, index=False, header=True)
     elif 'marketing_summary' in filename:
-        slice_column = 5
-        df = df.iloc[:, :slice_column]
-
-        output_dir = os.path.join("datasets", "cleaned")
-        os.makedirs(output_dir, exist_ok=True)
-        output_path = os.path.join(output_dir, f"{os.path.basename(file_path)}_cleaned_{datetime.date.today()}.csv")
-        df.to_csv(output_path, index=False, header=False)
+        clean_data = clean_marketing_summary.clean_marketing_summary(dataframe)[1:]
+        output_path = standardize_output_filename(file_path)
+        clean_data.to_csv(output_path, index=False, header=True)
     elif 'trend_report' in filename:
-        slice_column = 3
-        df = df.iloc[:, :slice_column]
+        clean_data = clean_trend_report.clean_trend_report(dataframe)[1:]
+        output_path = standardize_output_filename(file_path)
+        clean_data.to_csv(output_path, index=False, header=True)
 
+    return FileExistsError(f"File {output_path} already exists. Please choose a different name or delete the existing file.")
 
-        output_dir = os.path.join("datasets", "cleaned")
-        os.makedirs(output_dir, exist_ok=True)
-        output_path = os.path.join(output_dir, f"{os.path.basename(file_path)}_cleaned_{datetime.date.today()}.csv")
-        df.to_csv(output_path, index=False, header=False)
+def standardize_output_filename(file_path):
+    """
+    Standardizes the output filename by removing special characters and replacing spaces with underscores.
+    """
+    output_dir = os.path.join("datasets", "cleaned")
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, f"{os.path.basename(file_path)}_cleaned_{datetime.date.today()}.csv")
 
-    return df
+    return output_path
 
 
 if __name__ == "__main__":
---
+++ functions/clean_event_logs.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+# Function to clean event logs data
+
+def clean_event_logs(dataframe):
+
+    slice_column = 5
+    headers = ['user_id', 'event_type', 'event_time', 'product_id', 'amount']
+    df = pd.read_csv(dataframe, header=None, names=headers, usecols=range(slice_column))
+
+    # Check if the df is empty
+    if df.empty:
+        print("The df is empty.")
+        return df
+    else:
+        # Remove rows with all NaN values
+        cleaned_df = df.dropna(how='any')
+
+    cleaned_df['event_time'] = pd.to_datetime(cleaned_df['event_time'], errors='coerce')
+
+    #Separating date time into date time
+
+    cleaned_df['date'] = cleaned_df['event_time'].dt.date
+    cleaned_df['time'] = cleaned_df['event_time'].dt.time
+
+    # Drop the original column that contained both date and time
+
+    cleaned_df = cleaned_df.drop('event_time', axis=1)
+    
+    return cleaned_df
+
